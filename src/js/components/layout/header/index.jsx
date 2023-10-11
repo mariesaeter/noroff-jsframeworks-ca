c14d9457a91018c8eb66d0f8af89dede5e204fbd
mariesaeter <marie.saeter@gmail.com>
2023-10-11 11:11:34 +0200

    Created and styled hero-banner

--- src/js/components/layout/header/index.jsx
+++ src/js/components/layout/header/index.jsx
@@ -3,9 +3,9 @@ import { Nav } from "../nav";
 
 export function Header() {
   return (
-    <header className="p-l-20">
+    <header className="p-l-20 p-r-20">
       <Link to="/">
-        <img src="/logo.png" alt="trendtrove logo" />
+        <img src="/logo.png" alt="trendtrove logo" className="logo" />
       </Link>
       <Nav />
     </header>
--- src/js/components/layout/home/index.jsx
+++ src/js/components/layout/home/index.jsx
@@ -1,3 +1,21 @@
+function Banner() {
+  return (
+    <div className="hero-banner display-flex-column justify-content-between">
+      <div>
+        <div className="hero-banner-heading m-t-30">Welcome to</div>
+        <h1 className="hero-banner-title m-t-30"> TrendTrove</h1>
+        <p className="hero-banner-text m-t-20">The trendy fashion store</p>
+      </div>
+      <div className="hero-banner-line"></div>
+    </div>
+  );
+}
+
 export function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner />
+      Hi
+    </div>
+  );
 }
