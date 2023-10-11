28e3155736751b819240b42a44171ef62f5c764a
mariesaeter <marie.saeter@gmail.com>
2023-10-11 17:08:16 +0200

    Fetched api, added product card with styling on home page

--- src/js/components/layout/home/index.jsx
+++ src/js/components/layout/home/index.jsx
@@ -1,12 +1,54 @@
+import { useApi } from "../../../hooks/api";
+import { rating } from "../rating";
+
 function Banner() {
   return (
     <div className="hero-banner display-flex-column justify-content-between">
       <div>
         <div className="hero-banner-heading m-t-30">Welcome to</div>
-        <h1 className="hero-banner-title m-t-30"> TrendTrove</h1>
-        <p className="hero-banner-text m-t-20">The trendy fashion store</p>
+        <h1 className="hero-banner-title "> TrendTrove</h1>
+        <p className="hero-banner-text">The trendy fashion store</p>
       </div>
       <div className="hero-banner-line"></div>
+    </div>
+  );
+}
+const url = "https://api.noroff.dev/api/v1/online-shop";
+
+// function Product() {
+//   return (
+
+//   )
+// }
+
+function Products() {
+  const { products, isLoading, isError } = useApi(url);
+  if (isLoading) {
+    return <div>Loading products</div>;
+  }
+  if (isError) {
+    return <div>Error with loading products</div>;
+  }
+
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <div key={product.id} className="product-card">
+          <img
+            src={product.imageUrl}
+            alt={product.title}
+            className="product-card-img"
+          />
+          <h2 className="product-card-title">{product.title}</h2>
+          <span className="product-rating">
+            {rating(product.rating)} {product.rating}
+          </span>
+          <div className="center-flex product-card-price m-b-10">
+            <p className="m-r-10">{product.price} kr</p>
+            <p>{product.discountedPrice} kr</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
@@ -15,7 +57,10 @@ export function Home() {
   return (
     <div>
       <Banner />
-      Hi
+      <div className="m-l-20 m-r-20">
+        <h1>Products</h1>
+        <Products />
+      </div>
     </div>
   );
 }
---
+++ src/js/components/layout/rating.js
@@ -0,0 +1,78 @@
+export function rating(rating) {
+  const fullStar = (
+    <img src="/icons/full-star.png" alt="full star" className="m-5" />
+  );
+  const halfStar = (
+    <img src="/icons/half-star.png" alt="half star" className="m-5" />
+  );
+  const emptyStar = (
+    <img src="/icons/empty-star.png" alt="empty star" className="m-5" />
+  );
+
+  if (rating === 5) {
+    return (
+      <>
+        {fullStar}
+        {fullStar}
+        {fullStar}
+        {fullStar}
+        {fullStar}
+      </>
+    );
+  }
+  if (rating === 4.5) {
+    return (
+      <>
+        {fullStar}
+        {fullStar}
+        {fullStar}
+        {fullStar}
+        {halfStar}
+      </>
+    );
+  }
+  if (rating === 4) {
+    return (
+      <>
+        {fullStar}
+        {fullStar}
+        {fullStar}
+        {fullStar}
+        {emptyStar}
+      </>
+    );
+  }
+  if (rating === 3) {
+    return (
+      <>
+        {fullStar}
+        {fullStar}
+        {fullStar}
+        {emptyStar}
+        {emptyStar}
+      </>
+    );
+  }
+  if (rating === 2) {
+    return (
+      <>
+        {fullStar}
+        {fullStar}
+        {emptyStar}
+        {emptyStar}
+        {emptyStar}
+      </>
+    );
+  }
+  if (rating === 0) {
+    return (
+      <>
+        {emptyStar}
+        {emptyStar}
+        {emptyStar}
+        {emptyStar}
+        {emptyStar}
+      </>
+    );
+  }
+}
