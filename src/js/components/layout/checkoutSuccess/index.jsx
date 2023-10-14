c668831be07d187b60c889f7c430110cc47c9b2c
mariesaeter <marie.saeter@gmail.com>
2023-10-14 23:48:58 +0200

    Added content to checkout success page

--- src/js/components/layout/checkoutSuccess/index.jsx
+++ src/js/components/layout/checkoutSuccess/index.jsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
 export function CheckoutSuccess() {
-  return <div>Checkout succeeded</div>;
+  return (
+    <div className="m-l-20 m-r-20">
+      <h1>Order received!</h1>
+      <p>
+        Your order have been received successfully and will be shipped as soon
+        as possible!
+      </p>
+      <p>
+        If you have any further questions about your order, feel free to use our
+        contact form.
+      </p>
+      <Link to="/">
+        <button className="primary-button">Back to store</button>
+      </Link>
+    </div>
+  );
 }
